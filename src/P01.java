9ea77da213ea6d998b952866f48b9bab9b65b798
Kevss <kevsermarulcu@gmail.com>
2024-04-29 14:10:15 +0300

    first commit

---
+++ src/P01.java
@@ -0,0 +1,11 @@
+public class P01 {
+    public static void main(String[] args) {
+
+        System.out.println("merhaba github");
+        int a = 120;
+        int b = 180;
+        int c = a+b;
+        System.out.println("c = " + c);
+
+    }
+}
